d696155632597ac1a498089c15cef48555f9834b
DILEEP KUMAR PANDIYA <DILEEP.PANDIYA01@ad.Infosys.com>
2016-12-28 18:07:04 -0600

    add

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
-'use strict';
+var express = require('express');
+var app = express();
+var bodyparser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://' + process.env.MONGO_PORT_27017_TCP_ADDR + ':27017/dockerdemo';
+var db;
 
-const express = require('express');
-
-// Constants
-const PORT = 8080;
-
-// App
-const app = express();
-app.get('/', function (req, res) {
-  res.send('Hello world\n');
+MongoClient.connect(url, function (err, database) {
+    console.log("Connected correctly to server");
+    db = database;
 });
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+app.use(bodyparser.json());
+app.use(express.static('public'));
+
+var insertDocument = function (db, document, callback) {
+    // Get the documents collection
+    var collection = db.collection('documents');
+    // Insert some documents
+    collection.insertOne(document, function (err, result) {
+        callback(err, JSON.stringify(result.ops[0]));
+    });
+};
+
+app.post('/hello', function (req, res) {
+    var data = req.body;
+    insertDocument(db, data, function(err, result) {
+        res.status(201).send(result)
+    })
+});
+
+app.get('/hello', function (req, res) {
+    res.send('world');
+});
+
+app.listen(8080);
